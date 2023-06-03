343dc236a7ae5f2d5b1b21572c445f799356dc03
DragonNexa <tirtha070@gmail.com>
2023-06-03 21:57:01 +0530

    Added progress ss

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -1,17 +1,7 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:smart_city_dashboard/constants/text_styles.dart';
 import 'package:smart_city_dashboard/pages/dashboard/right_panel.dart';
-import 'package:smart_city_dashboard/providers/page_providers.dart';
-import 'package:smart_city_dashboard/widgets/extensions.dart';
 import 'package:smart_city_dashboard/widgets/helper.dart';
-
 import '../../constants/constants.dart';
-import '../../constants/texts.dart';
-import '../../constants/theme.dart';
-import '../../ssh_lg/ssh.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
@@ -28,12 +18,12 @@ class Dashboard extends StatelessWidget {
             const SizedBox.shrink(),
             Container(
               width: (screenSize(context).width - Const.tabBarWidth) / 2,
-              color: Colors.blue,
+              // color: Colors.blue,
               child: Column(),
             ),
             Container(
                 width: (screenSize(context).width - Const.tabBarWidth) / 2 - 40,
-                color: Colors.blue,
+                // color: Colors.blue,
                 child: const RightPanel()),
             const SizedBox.shrink(),
           ],
