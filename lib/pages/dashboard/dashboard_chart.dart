8dbd065dcf92cb006907604612374d49ebba14b9
DragonNexa <tirtha070@gmail.com>
2023-06-10 01:25:59 +0530

    Added FL Chart package

---
+++ lib/pages/dashboard/dashboard_chart.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:smart_city_dashboard/constants/text_styles.dart';
+import 'package:smart_city_dashboard/models/forecast_weather.dart';
+import 'package:smart_city_dashboard/models/realtime_weather.dart';
+import 'package:smart_city_dashboard/providers/page_providers.dart';
+import 'package:smart_city_dashboard/services/weather_api.dart';
+import 'package:smart_city_dashboard/widgets/extensions.dart';
+import 'package:smart_city_dashboard/widgets/helper.dart';
+import 'package:smart_city_dashboard/widgets/logo_shower.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+import '../../constants/constants.dart';
+import '../../constants/texts.dart';
+import '../../constants/theme.dart';
+import '../../providers/settings_providers.dart';
+import '../../ssh_lg/ssh.dart';
