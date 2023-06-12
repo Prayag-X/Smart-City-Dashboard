8150c8c7ebf148c62064b722769658f8a76e6604
DragonNexa <tirtha070@gmail.com>
2023-06-12 19:42:53 +0530

    Added home page animation

--- lib/pages/homepages/home_page.dart
+++ lib/pages/homepages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:smart_city_dashboard/constants/available_cities.dart';
 import 'package:smart_city_dashboard/pages/homepages/city_card.dart';
 import 'package:smart_city_dashboard/widgets/helper.dart';
+
+import '../../constants/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -15,13 +18,24 @@ class HomePage extends StatelessWidget {
           const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
       child: SizedBox(
         height: screenSize(context).height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: CityCardData.availableCities
-                .map((city) => CityCard(
-                      cityData: city,
-                    ))
-                .toList()),
+        child: AnimationLimiter(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: AnimationConfiguration.toStaggeredList(
+                duration: Const.animationDuration,
+                childAnimationBuilder: (widget) => SlideAnimation(
+                  verticalOffset: Const.animationDistance,
+                  child: FadeInAnimation(
+                    child: widget,
+                  ),
+                ),
+                children: CityCardData.availableCities
+                    .map((city) => CityCard(
+                          cityData: city,
+                        ))
+                    .toList()),
+          ),
+        ),
       ),
     );
   }
