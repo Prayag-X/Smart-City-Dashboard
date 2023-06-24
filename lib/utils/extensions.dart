b4fe6810ae3cc98c8a73e0dd85dfc389b3d22291
DragonNexa <tirtha070@gmail.com>
2023-06-24 21:49:50 +0530

    Added comments in extensions

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -9,24 +9,30 @@ extension SizedBoxPadding on num {
 }
 
 extension ZoomLG on num {
-  double get zoomLG => Const.lgZoomScale / pow(2, this);
+  double get zoomLG =>
+      Const.lgZoomScale / pow(2, this); // Formula to match zoom of GMap with LG
 }
 
 extension SetPercentage on num {
-  double get setTemperaturePercentage => min(max((this + 30) / 85, 0), 1);
-  double get setPressurePercentage => min(max((this - 800) / 500, 0), 1);
-  double get setUVPercentage => this / 15;
-  double get setPercentage => this / 100;
-  double get setPercentageAbout => this / 10000;
+  double get setTemperaturePercentage => min(max((this + 30) / 85, 0),
+      1); // Lowest temp = -30, highest +55, total variation = 85
+  double get setPressurePercentage => min(max((this - 800) / 500, 0),
+      1); // Lowest pressure +800, highest +1300, total variation = 500
+  double get setUVPercentage => this / 15; // Max uv = 15
+  double get setPercentage => this / 100; // Max 100
+  double get setPercentageAbout => this / 10000; // Max point = 10000
 }
 
 extension Parser on String {
   String get parseIcon =>
-      '${ImageConst.allWeatherLogos}${split('/').reversed.elementAt(1)}/${split('/').last}';
-  String get parseTime => split(' ').last;
-  String get parseDay => split(' ')[0].split('-')[2];
+      '${ImageConst.allWeatherLogos}${split('/').reversed.elementAt(1)}/${split('/').last}'; // Return a string like this "assets/logos/weather/day/mist.png"
+  String get parseTime =>
+      split(' ').last; // Parse time from "MM-DD-YYYY TT:TT" format
+  String get parseDay =>
+      split(' ')[0].split('-')[2]; // Parse day from "MM-DD-YYYY TT:TT" format
 }
 
+// Add neumorphism style to widgets
 extension Neumorphism on Widget {
   addNeumorphism({
     double borderRadius = 10.0,
