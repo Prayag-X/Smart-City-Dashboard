f1b4ec0926a207c583589579f4d98a577ebd3a58
DragonNexa <tirtha070@gmail.com>
2023-06-29 14:24:18 +0530

    Fixed #6

---
+++ lib/pages/dashboard/widgets/dashboard_pie_chart.dart
@@ -0,0 +1,126 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class PieChartSample2 extends StatefulWidget {
+  const PieChartSample2({super.key});
+
+  @override
+  State<StatefulWidget> createState() => PieChart2State();
+}
+
+class PieChart2State extends State {
+  int touchedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.3,
+      child: Row(
+        children: <Widget>[
+          const SizedBox(
+            height: 18,
+          ),
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: PieChart(
+                PieChartData(
+                  pieTouchData: PieTouchData(
+                    touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                      setState(() {
+                        if (!event.isInterestedForInteractions ||
+                            pieTouchResponse == null ||
+                            pieTouchResponse.touchedSection == null) {
+                          touchedIndex = -1;
+                          return;
+                        }
+                        touchedIndex = pieTouchResponse
+                            .touchedSection!.touchedSectionIndex;
+                      });
+                    },
+                  ),
+                  borderData: FlBorderData(
+                    show: false,
+                  ),
+                  sectionsSpace: 0,
+                  centerSpaceRadius: 40,
+                  sections: List.generate(4, (i) {
+                    final isTouched = i == touchedIndex;
+                    final fontSize = isTouched ? 25.0 : 16.0;
+                    final radius = isTouched ? 60.0 : 50.0;
+                    const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
+                    switch (i) {
+                      case 0:
+                        return PieChartSectionData(
+                          color: Colors.blue,
+                          value: 40,
+                          title: '40%',
+                          radius: radius,
+                          titleStyle: TextStyle(
+                            fontSize: fontSize,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            shadows: shadows,
+                          ),
+                        );
+                      case 1:
+                        return PieChartSectionData(
+                          color: Colors.yellow,
+                          value: 30,
+                          title: '30%',
+                          radius: radius,
+                          titleStyle: TextStyle(
+                            fontSize: fontSize,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            shadows: shadows,
+                          ),
+                        );
+                      case 2:
+                        return PieChartSectionData(
+                          color: Colors.purple,
+                          value: 15,
+                          title: '15%',
+                          radius: radius,
+                          titleStyle: TextStyle(
+                            fontSize: fontSize,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            shadows: shadows,
+                          ),
+                        );
+                      case 3:
+                        return PieChartSectionData(
+                          color: Colors.green,
+                          value: 15,
+                          title: '15%',
+                          radius: radius,
+                          titleStyle: TextStyle(
+                            fontSize: fontSize,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            shadows: shadows,
+                          ),
+                        );
+                      default:
+                        throw Error();
+                    }
+                  }),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const <Widget>[
+            ],
+          ),
+          const SizedBox(
+            width: 28,
+          ),
+        ],
+      ),
+    );
+  }
+}
